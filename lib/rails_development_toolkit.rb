4c8a5cf95879046ddc5e7cb8f46f6d8cca93f58a
ingeniarius <mail@ingeniarius.name>
2012-01-02 22:55:36 +0400

    Autoload rack development mode module

--- lib/rails_development_toolkit.rb
+++ lib/rails_development_toolkit.rb
@@ -1,2 +1,6 @@
 require 'rails_development_toolkit/version'
 require 'rails_development_toolkit/railtie' if defined?(Rails)
+
+module Rack
+  autoload :DevelopmentMode, "rack/development_mode"
+end  
--- lib/rails_development_toolkit/version.rb
+++ lib/rails_development_toolkit/version.rb
@@ -1,3 +1,3 @@
 module RailsDevelopmentToolkit
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
